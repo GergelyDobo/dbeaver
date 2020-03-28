35ea7a0f629b08667413906880d28366c9b98c5b
Dobó Gergely <h754939@stud.u-szeged.hu>
2020-03-28 21:34:41 +0100

    #253 Extend SQLEditorMessages with action_SelectAllFrom_label

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/internal/SQLEditorMessages.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/internal/SQLEditorMessages.java
@@ -93,6 +93,8 @@ public class SQLEditorMessages extends NLS {
     public static String action_popup_sqleditor_layout_vertical;
     public static String action_popup_sqleditor_layout_detached;
 
+    public static String action_SelectAllFrom_label;
+
     public static String actions_ContentAssistProposal_label;
     public static String actions_ContentAssistProposal_tooltip;
     public static String actions_ContentAssistProposal_description;
